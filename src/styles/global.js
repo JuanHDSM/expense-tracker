f7fcc2fa8c39b48d20118d725138af633c117371
Juan Holy <juanholydesouza@gmail.com>
2023-01-20 01:26:09 -0300

    Primeiro Commit

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+  background-color: #F2F2F2;
+}
+`;
+export default GlobalStyle;
